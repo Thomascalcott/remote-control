10d7d3b490e832c30fcdbb737fe93e2bb9d32466
Thomascalcott <78829276+Thomascalcott@users.noreply.github.com>
2021-02-17 12:35:24 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onGesture(Gesture.LogoUp, function () {
-    radio.sendString("Down")
+    radio.sendString("Back")
 })
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendString("Left")
@@ -9,9 +9,9 @@ radio.onReceivedString(function (receivedString) {
         basic.showArrow(ArrowNames.West)
     } else if (receivedString == "Right") {
         basic.showArrow(ArrowNames.East)
-    } else if (receivedString == "Up") {
+    } else if (receivedString == "Forward") {
         basic.showArrow(ArrowNames.North)
-    } else if (receivedString == "Down") {
+    } else if (receivedString == "Back") {
         basic.showArrow(ArrowNames.South)
     }
 })
@@ -19,7 +19,7 @@ input.onGesture(Gesture.TiltRight, function () {
     radio.sendString("Right")
 })
 input.onGesture(Gesture.LogoDown, function () {
-    radio.sendString("Up")
+    radio.sendString("Forward")
 })
 basic.showIcon(IconNames.Heart)
 radio.setGroup(69)
